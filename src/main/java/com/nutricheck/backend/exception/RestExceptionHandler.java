41ea3bbb5718d3d69971210f66c4ee63b54bb075
mizarion <zsgluschenko@edu.hse.ru>
2025-11-04 23:47:51 +0900

    #1 add ControllerAdvice

---
+++ src/main/java/com/nutricheck/backend/exception/RestExceptionHandler.java
@@ -0,0 +1,57 @@
+package com.nutricheck.backend.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RestExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ErrorDto> defaultExceptionHandler(HttpServletRequest req, Exception e) {
+        var error = ErrorDto.builder()
+                .timestamp(LocalDateTime.now())
+                .path(req.getContextPath() + req.getServletPath())
+                .message(e.getLocalizedMessage());
+
+        if (e instanceof MissingServletRequestParameterException
+//                   || e instanceof UsernameNotFoundException
+//                   || e instanceof BadCredentialsException
+            || e instanceof HttpMessageNotReadableException
+            || e instanceof ConstraintViolationException) {
+            return new ResponseEntity<>(error.build(), HttpStatus.NOT_ACCEPTABLE);
+        } else {
+            return new ResponseEntity<>(error.build(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDto> MethodArgumentNotValidExceptionHandler(HttpServletRequest req, MethodArgumentNotValidException ex) {
+        List<String> missingParams = ex
+                .getBindingResult()
+                .getFieldErrors().stream()
+                .map(param -> "Missing param in JSON '" + param.getField() + "'. " + param.getDefaultMessage())
+                .toList();
+        ErrorDto errorDto = ErrorDto.builder()
+                .timestamp(LocalDateTime.now())
+                .path(req.getContextPath() + req.getServletPath())
+                .message(missingParams.toString())
+                .build();
+        return new ResponseEntity<>(errorDto, HttpStatus.NOT_ACCEPTABLE);
+    }
+}
+
